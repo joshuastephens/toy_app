31af2169cf653c52a749c7d360d5aa36c915c2d7
joshuastephens <joshua.stephens@arcadiasolutions.com>
2016-01-21 23:25:39 +0000

    possible fix for route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   resources :users
+   root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   resources :users
-   root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
